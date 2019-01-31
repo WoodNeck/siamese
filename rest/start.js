6003508e82b2bb7ebc72dfe7266819bee19d04f0
WoodNeck <saiyu915@naver.com>
2019-02-01 00:04:13 +0900

    feat: Api server - file management role checking

--- rest/start.js
+++ rest/start.js
@@ -3,13 +3,19 @@ const express = require('express');
 module.exports = bot => {
 	const app = express();
 
+	// CORS
+	app.use((req, res, next) => {
+		res.append('Access-Control-Allow-Origin', ['*']);
+		res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+		res.append('Access-Control-Allow-Headers', 'Content-Type');
+		next();
+	});
+
 	// Return user guilds
 	app.get('/guilds', (req, res) => {
-		const userId = req.query.user.toString();
+		const userId = req.query.user;
 
-		const guilds = bot.guilds.filter(guild => {
-			return guild.members.some(user => user.id === userId);
-		});
+		const guilds = bot.guilds.filter(guild => guild.members.has(userId));
 
 		const formattedGuild = guilds.map(guild => {
 			return {
@@ -20,6 +26,39 @@ module.exports = bot => {
 		});
 
 		res.send(JSON.stringify(formattedGuild));
+	});
+
+	// Return user permission for guild
+	app.get('/role', (req, res) => {
+		const guildId = req.query.guild;
+		const userId = req.query.user;
+
+		const guild = bot.guilds.get(guildId);
+
+		if (guild) {
+			const user = guild.members.get(userId);
+			if (!user) {
+				res.send({});
+				return;
+			}
+
+			const hasPermission = guild.roles.some(
+				role => {
+					return role.name === global.env.FILE_MANAGEMENT_ROLE_NAME
+						&& user.roles.has(role.id);
+				},
+			);
+
+			res.send({
+				id: guild.id,
+				iconURL: `https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}.png`,
+				name: guild.name,
+				hasPermission,
+			});
+		}
+		else {
+			res.send({});
+		}
 	});
 
 	app.listen(4260, () => {
