54701cdb02c8ec5a79fa9643d55c94a75640763d
WoodNeck <saiyu915@naver.com>
2021-03-14 21:45:19 +0900

    web: can't login on iOS

--- src/api/get/login.ts
+++ src/api/get/login.ts
@@ -3,7 +3,9 @@ import passport from "passport";
 import { Register } from "../register";
 
 const register: Register = ({ app, bot }) => {
-  app.get("/auth/discord", passport.authenticate("discord"));
+  app.get("/auth/discord", passport.authenticate("discord", {
+    session: true
+  }));
   app.get("/auth/discord/callback", passport.authenticate("discord", {
     failureRedirect: `${bot.env.WEB_URL_BASE}/fail`,
     session: true
