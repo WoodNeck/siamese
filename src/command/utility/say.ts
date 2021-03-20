5d889ad15630ead1947de3b634ebc66e4d1a34ef
WoodNeck <saiyu915@naver.com>
2021-03-20 16:55:28 +0900

    fix: say should have manage message permission

--- src/command/utility/say.ts
+++ src/command/utility/say.ts
@@ -1,11 +1,15 @@
 import Command from "~/core/Command";
 import { SAY } from "~/const/command/utility";
 import * as ERROR from "~/const/error";
+import * as PERMISSION from "~/const/permission";
 
 export default new Command({
   name: SAY.CMD,
   description: SAY.DESC,
   usage: SAY.USAGE,
+  permissions: [
+    PERMISSION.MANAGE_MESSAGES
+  ],
   execute: async ({ bot, msg, channel, content }) => {
     // Can't react for empty content
     if (!content) {
