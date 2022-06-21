fc198e5c6d0911d94c32387db48e5cf2e721e05e
WoodNeck <saiyu915@naver.com>
2022-06-21 21:56:07 +0900

    fix: remove cmd log

--- src/Siamese.ts
+++ src/Siamese.ts
@@ -41,7 +41,6 @@ class Siamese extends Discord.Client {
   private _msgCounts: Discord.Collection<string, number>;
   private _logger: ChannelLogger | ConsoleLogger;
   private _fileLogger: pino.Logger;
-  private _commandLogger: pino.Logger;
 
   public get env() { return this._env; }
   public get prefix() { return this._env.BOT_DEFAULT_PREFIX; }
@@ -70,7 +69,6 @@ class Siamese extends Discord.Client {
     this._cooldowns = new Discord.Collection();
     this._msgCounts = new Discord.Collection();
     this._fileLogger = logger;
-    this._commandLogger = pino({ prettyPrint: { translateTime: "SYS:standard" } }, pino.destination("./command.log"));
   }
 
   public async setup() {
@@ -413,7 +411,6 @@ class Siamese extends Discord.Client {
         await ctx.channel.sendTyping();
       }
 
-      this._commandLogger.info(`CMD: ${cmdName} ${ctx.content}`);
       await cmd.execute(ctx);
     } catch (err) {
       await this.handleError(ctx, cmd, err);
@@ -486,7 +483,6 @@ class Siamese extends Discord.Client {
     if (isOnCoolDown) return;
 
     try {
-      this._commandLogger.info(`SLASH: ${ctx.command.name} ${ctx.interaction.options.data.map(option => option.value).join(" / ")}`);
       await cmd.execute(ctx);
     } catch (err) {
       console.error(err);
